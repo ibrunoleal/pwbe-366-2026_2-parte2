1bc345f1edf63313b8eab3f992ad88713a32bd11
ibrunoleal <ibrunoleal@gmail.com>
2026-09-22 15:04:00 -0300

    modified:   controllers/aluno_controller.py 	modified:   routers/aluno_router.py 	modified:   static/css/estilos.css 	modified:   static/css/grid12.css 	modified:   static/css/navbar.css 	new file:   static/css/table.css 	modified:   templates/aluno/alunos.html 	modified:   templates/aluno/novo_aluno.html 	modified:   templates/base.html 	modified:   templates/index.html 	modified:   templates/partials/_navbar.html

--- controllers/aluno_controller.py
+++ controllers/aluno_controller.py
@@ -1,4 +1,22 @@
-from flask import render_template
+from flask import render_template, request, redirect
+
+lista_de_alunos = []
 
 def novo():
     return render_template('/aluno/novo_aluno.html')
+
+def salvar():
+    matricula = request.form.get('matricula')
+    nome = request.form.get('nome')
+    novo_aluno = {
+        'matricula': matricula,
+        'nome': nome
+    }
+    lista_de_alunos.append(novo_aluno)
+    return redirect('/aluno/alunos')
+
+def listar_alunos():
+    contexto = {
+        'alunos': lista_de_alunos
+    }
+    return render_template('/aluno/alunos.html', context=contexto)
--- routers/aluno_router.py
+++ routers/aluno_router.py
@@ -3,3 +3,5 @@ from controllers import aluno_controller
 
 def adicionar_rotas(app: Flask):
     app.add_url_rule('/aluno/novo', view_func=aluno_controller.novo, methods=['GET'])
+    app.add_url_rule('/aluno/salvar', view_func=aluno_controller.salvar, methods=['POST'])
+    app.add_url_rule('/aluno/alunos', view_func=aluno_controller.listar_alunos, methods=['GET'])
